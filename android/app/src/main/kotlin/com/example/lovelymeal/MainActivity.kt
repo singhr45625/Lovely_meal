04662230477b04a756dcfc3e8b518628d7daeecf
singhr45625 <kpuja0969@gmail.com>
2025-03-30 16:02:58 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/lovelymeal/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lovelymeal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/widgets/Big_text.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BigText extends StatelessWidget {
+  const BigText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
